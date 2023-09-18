3974d804c54bc7caa4aac2e45b75c7495a090203
Oscar Lopez <53553460+oscar-dev19@users.noreply.github.com>
2023-09-17 19:06:31 -0700

    refactored app as a streamlit app

--- app.py
+++ app.py
@@ -1,77 +1,32 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
 
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
+st.title("401(k) Investment Forecast")
 
-app = dash.Dash(__name__)
+# Input fields for 401(k)
+age = st.number_input("Age:", min_value=0, value=30)
+initial_contribution = st.number_input("Initial Contribution (﹩):", min_value=0, value=10000)
+contribution_increase = st.number_input("Annual Contribution Increase (%):", min_value=0, value=3)
+employer_match = st.number_input("Employer Match (%):", min_value=0, value=3)  # New input for employer match
 
-app.layout = html.Div([
-    html.H1("401(k) Investment Forecast"),
-    # Input fields for 401(k)
-    html.Label("Age:"),
-    dcc.Input(id="age-input", type="number", value=30),
-    html.Label("Initial Contribution ($):"),
-    dcc.Input(id="initial-contribution-input", type="number", value=10000),
-    html.Label("Annual Contribution Increase (%):"),
-    dcc.Input(id="contribution-increase-input", type="number", value=3),
-    html.Label("Employer Match (%):"),  # New input for employer match
-    dcc.Input(id="employer-match-input", type="number", value=3),  # Default to 3% match
-    # Output graph to display results
-    dcc.Graph(id="investment-forecast-graph"),
-])
+# Calculate 401(k) investment forecast with employer match
+years_to_retirement = 65 - age
+investment_forecast = [initial_contribution]
 
-def calculate_investment_forecast(age, initial_contribution, contribution_increase, employer_match):
-    # Validate inputs and provide default values if necessary
-    age = max(age, 0) if age is not None else 30
-    initial_contribution = max(initial_contribution, 0) if initial_contribution is not None else 10000
-    contribution_increase = max(contribution_increase, 0) if contribution_increase is not None else 3
-    employer_match = max(employer_match, 0) if employer_match is not None else 3
+for year in range(1, years_to_retirement + 1):
+    employer_contribution = investment_forecast[-1] * (employer_match / 100)
+    investment_forecast.append(
+        investment_forecast[-1] * (1 + contribution_increase / 100) + employer_contribution
+    )
 
-    # Calculate 401(k) investment forecast with employer match
-    years_to_retirement = 65 - age
-    investment_forecast = [initial_contribution]
+x_values = list(range(age, 66))
+y_values = investment_forecast
 
-    for year in range(1, years_to_retirement + 1):
-        employer_contribution = investment_forecast[-1] * (employer_match / 100)
-        investment_forecast.append(
-            investment_forecast[-1] * (1 + contribution_increase / 100) + employer_contribution
-        )
+# Create a line chart
+fig = px.line(x=x_values, y=y_values, labels={"x": "Age", "y": "Balance (﹩)"})
+fig.update_layout(title="401(k) Investment Forecast")
 
-    x_values = list(range(age, 66))
-    y_values = investment_forecast
+# Display the chart
+st.plotly_chart(fig)
 
-    return x_values, y_values
-
-@app.callback(
-    Output("investment-forecast-graph", "figure"),
-    [
-        Input("age-input", "value"),
-        Input("initial-contribution-input", "value"),
-        Input("contribution-increase-input", "value"),
-        Input("employer-match-input", "value"),  # New input for employer match
-    ],
-)
-def update_investment_forecast_graph(age, initial_contribution, contribution_increase, employer_match):
-    x_values, y_values = calculate_investment_forecast(age, initial_contribution, contribution_increase, employer_match)
-
-    fig = {
-        'data': [
-            {
-                'x': x_values,
-                'y': y_values,
-                'type': 'line',
-                'mode': 'lines+markers',
-                'name': '401(k) Forecast',
-            },
-        ],
-        'layout': {
-            'title': '401(k) Investment Forecast',
-            'xaxis': {'title': 'Age'},
-            'yaxis': {'title': 'Balance ($)'},
-        }
-    }
-
-    return fig
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
